223a4a26e2a90bb6897356ff11e41cee64dae634
Gusnand <72637679+Gusnand@users.noreply.github.com>
2022-10-27 15:46:22 +0800

    Update Kali.java

--- src/logic/Kali.java
+++ src/logic/Kali.java
@@ -1,5 +1,25 @@
 package logic;
 
 public class Kali {
-    
+    double multiple;
+
+    public Kali (double x, double y){
+        this.multiple = proses(x,y);
+    }
+    public Kali (String x){
+        var arr = x.split(" ");
+        this.multiple = (Double.parseDouble(arr[0])*Double.parseDouble((arr[1])));
+    }
+    double proses(int x, int y){
+        return (double) x*y;
+    }
+    double proses(double y, double x){
+        return x*y;
+    }
+
+    double proses(String x){
+        var arr = x.split(" ");
+        return Double.parseDouble(arr[0])* Double.parseDouble(arr[1]);
+    }
 }
+
